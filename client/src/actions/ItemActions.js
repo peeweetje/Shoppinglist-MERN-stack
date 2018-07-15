49b7f7293f450cd54699c821ee1d23b33e859afe
Petra Vos <petravos70@gmail.com>
2018-07-15 13:39:00 +0200

    refactor: add axios to file. Include api request.

--- client/src/actions/ItemActions.js
+++ client/src/actions/ItemActions.js
@@ -1,21 +1,36 @@
-import { GET_ITEMS, ADD_ITEMS, DELETE_ITEMS } from "./Types";
+import axios from "axios";
+import { GET_ITEMS, ADD_ITEMS, DELETE_ITEMS, ITEMS_LOADING } from "./Types";
 
-export const getItems = () => {
-  return {
-    type: GET_ITEMS
-  };
+export const getItems = () => dispatch => {
+  dispatch(setItemsLoading());
+  axios.get("/api/items").then(res =>
+    dispatch({
+      type: GET_ITEMS,
+      payload: res.data
+    })
+  );
 };
 
-export const deleteItems = id => {
-  return {
-    type: DELETE_ITEMS,
-    payload: id
-  };
+export const addItems = item => dispatch => {
+  axios.post("/api/items", item).then(res =>
+    dispatch({
+      type: ADD_ITEMS,
+      payload: res.data
+    })
+  );
 };
 
-export const addItems = item => {
+export const deleteItems = id => dispatch => {
+  axios.delete(`/api/items/${id}`).then(res =>
+    dispatch({
+      type: DELETE_ITEMS,
+      payload: id
+    })
+  );
+};
+
+export const setItemsLoading = () => {
   return {
-    type: ADD_ITEMS,
-    payload: item
+    type: ITEMS_LOADING
   };
 };
