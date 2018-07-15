1826b48b7662fc18110ef0924344f9c215f40b8e
Petra Vos <petravos70@gmail.com>
2018-07-15 13:37:46 +0200

    refactor: remove uuid from file. remove hardcoded code from file. Add items_loading to file.

--- client/src/reducers/itemReducers.js
+++ client/src/reducers/itemReducers.js
@@ -1,30 +1,37 @@
-import uuid from "uuid";
-import { GET_ITEMS, ADD_ITEMS, DELETE_ITEMS } from "../actions/Types";
+import {
+  GET_ITEMS,
+  ADD_ITEMS,
+  DELETE_ITEMS,
+  ITEMS_LOADING
+} from "../actions/Types";
 
 const initialState = {
-  items: [
-    { id: uuid(), name: "Noah" },
-    { id: uuid(), name: "Dushi" },
-    { id: uuid(), name: "Keepwee" },
-    { id: uuid(), name: "Dodger" }
-  ]
+  items: [],
+  loading: false
 };
 
 export default function(state = initialState, action) {
   switch (action.type) {
     case GET_ITEMS:
       return {
-        ...state
+        ...state,
+        items: action.payload,
+        loading: false
       };
     case DELETE_ITEMS:
       return {
         ...state,
-        items: state.items.filter(item => item.id !== action.payload)
+        items: state.items.filter(item => item._id !== action.payload)
       };
     case ADD_ITEMS:
       return {
         ...state,
         items: [action.payload, ...state.items]
+      };
+    case ITEMS_LOADING:
+      return {
+        ...state,
+        loading: true
       };
     default:
       return state;
